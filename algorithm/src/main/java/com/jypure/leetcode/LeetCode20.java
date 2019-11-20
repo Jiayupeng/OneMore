ee0923ba00d3f8255ffaf0c33476ece7466379aa
jiayupeng <jiayupeng@meicai.cn>
2019-11-20 20:51:10 +0800

    leetcode20

---
+++ algorithm/src/main/java/com/jypure/leetcode/LeetCode20.java
@@ -0,0 +1,51 @@
+package com.jypure.leetcode;
+
+import java.util.Stack;
+
+/**
+ * 有效的括号
+ * @author : jiayupeng
+ * @date : 2019/11/20/19:36
+ */
+public class LeetCode20 {
+
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     *
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 注意空字符串可被认为是有效字符串。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/valid-parentheses
+     * @param s
+     * @return
+     */
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == ']' && topChar != '[') {
+                    return false;
+                }
+                if (c == '}' && topChar != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
