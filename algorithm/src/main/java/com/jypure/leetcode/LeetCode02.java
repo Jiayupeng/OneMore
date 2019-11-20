5fd68f55c5765aab8ea381c77965f51308ef250f
jiayupeng <jiayupeng@meicai.cn>
2019-11-20 19:27:50 +0800

    leetcode02

---
+++ algorithm/src/main/java/com/jypure/leetcode/LeetCode02.java
@@ -0,0 +1,88 @@
+package com.jypure.leetcode;
+
+/**
+ * 两数相加
+ * @author : jiayupeng
+ * @date : 2019/11/20/17:16
+ */
+public class LeetCode02 {
+
+    /**
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     *
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     *
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10 );
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+        l1.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+        l2.next.next = new ListNode(9);
+        l2.next.next.next = new ListNode(9);
+        l2.next.next.next.next = new ListNode(9);
+        l2.next.next.next.next.next = new ListNode(9);
+        l2.next.next.next.next.next.next = new ListNode(9);
+        l2.next.next.next.next.next.next.next = new ListNode(9);
+        l2.next.next.next.next.next.next.next.next = new ListNode(9);
+        l2.next.next.next.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode listNode = addTwoNumbers(l1, l2);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+
+    }
+
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){val = x;}
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
