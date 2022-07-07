4bdd68691dd648c1c919aa555ca900886592eb18
jiayupeng <jyp901@hotmail.com>
2022-07-07 09:44:15 +0800

    code24

--- algorithm/src/main/java/com/jypure/leetcode/Code24.java
+++ algorithm/src/main/java/com/jypure/leetcode/Code24.java
@@ -35,4 +35,20 @@ public class Code24 {
 
     }
 
+    public ListNode swapPairs1(ListNode head) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode prev = dummyNode;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode temp = head.next.next;
+            prev.next = head.next;
+            head.next.next = head;
+            head.next = temp;
+            prev = head;
+            head = head.next;
+        }
+        return dummyNode.next;
+    }
+
 }
