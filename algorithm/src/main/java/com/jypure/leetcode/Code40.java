6eb2b7fe6379cd265acd3bf985b3f0e940b49409
jiayupeng <jyp901@hotmail.com>
2022-07-21 16:36:54 +0800

    二叉树的最大深度

---
+++ algorithm/src/main/java/com/jypure/leetcode/Code40.java
@@ -0,0 +1,47 @@
+package com.jypure.leetcode;
+
+import com.jypure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 二叉树的最大深度
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+ * @author : jiayupeng
+ * @date : 2022/7/21/16:27
+ */
+public class Code40 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> que = new LinkedList<>();
+
+        que.offer(root);
+
+        int deep = 0;
+        while (!que.isEmpty()) {
+            int len = que.size();
+            while (len > 0) {
+                TreeNode poll = que.poll();
+                if (poll.left != null) {
+                    que.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    que.offer(poll.right);
+                }
+                len--;
+            }
+            deep++;
+        }
+
+
+        return deep;
+
+    }
+
+
+}
