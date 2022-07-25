d46d500b98f428569a51b8be6f6a09c0fe032f76
jiayupeng <jyp901@hotmail.com>
2022-07-25 11:00:53 +0800

    翻转二叉树

--- algorithm/src/main/java/com/jypure/leetcode/Code42.java
+++ algorithm/src/main/java/com/jypure/leetcode/Code42.java
@@ -2,6 +2,8 @@ package com.jypure.leetcode;
 
 import com.jypure.TreeNode;
 
+import java.util.ArrayDeque;
+
 /**
  * 翻转二叉树
  * https://leetcode.cn/problems/invert-binary-tree/
@@ -10,6 +12,10 @@ import com.jypure.TreeNode;
  */
 public class Code42 {
 
+    /**
+     * 前后序遍历都可以
+     * 中序不行，因为先左孩子交换孩子，再根交换孩子（做完后，右孩子已经变成了原来的左孩子），再右孩子交换孩子（此时其实是对原来的左孩子做交换）
+     */
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
@@ -27,5 +33,41 @@ public class Code42 {
         node.left = node.right;
         node.right = tmp;
     }
+
+
+    /**
+     * BFS
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree1(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        ArrayDeque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            while (size-- > 0) {
+                TreeNode node = deque.poll();
+                swap(node);
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    public void swap(TreeNode root) {
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+
+
 
 }
