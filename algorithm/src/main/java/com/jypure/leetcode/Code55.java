48d03e74252e5efda35fd34403fd35a9a2c2444e
jiayupeng <jyp901@hotmail.com>
2022-08-01 22:07:13 +0800

    617. 合并二叉树

--- algorithm/src/main/java/com/jypure/leetcode/Code55.java
+++ algorithm/src/main/java/com/jypure/leetcode/Code55.java
@@ -2,14 +2,127 @@ package com.jypure.leetcode;
 
 import com.jypure.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
+ * 617. 合并二叉树
+ * https://leetcode.cn/problems/merge-two-binary-trees/
  * @author : jiayupeng
  * @date : 2022/8/1/20:02
  */
 public class Code55 {
 
+    /**
+     * 递归三部曲，1，确认入参和返回值，2确定终止条件，3确定单层逻辑
+     * @param root1
+     * @param root2
+     * @return
+     */
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
 
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
     }
+
+    /**
+     * 使用栈迭代
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root2);
+        stack.push(root1);
+
+        while (!stack.isEmpty()) {
+            TreeNode node1 = stack.pop();
+            TreeNode node2 = stack.pop();
+            node1.val += node2.val;
+            if (node1.left != null && node2.left != null) {
+                stack.push(node2.left);
+                stack.push(node1.left);
+            } else {
+                if (node1.left == null) {
+                    node1.left = node2.left;
+                }
+            }
+
+            if (node1.right != null && node2.right != null) {
+                stack.push(node2.right);
+                stack.push(node1.right);
+            } else {
+                if (node1.right == null) {
+                    node1.right = node2.right;
+                }
+            }
+
+        }
+
+        return root1;
+
+    }
+
+    /**
+     * 使用队列迭代
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 ==null) return root1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root1);
+        queue.offer(root2);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+
+            node1.val += node2.val;
+
+            if (node1.left != null && node2.left != null) {
+                queue.offer(node1.left);
+                queue.offer(node2.left);
+            }
+
+            if (node1.right != null && node2.right != null) {
+                queue.offer(node1.right);
+                queue.offer(node2.right);
+            }
+
+            if (node1.left == null) {
+                node1.left = node2.left;
+            }
+
+            if (node1.right == null) {
+                node1.right = node2.right;
+            }
+        }
+
+        return root1;
+    }
+
+
+
 
 }
