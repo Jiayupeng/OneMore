2fd1a57ee25e3df8b62e99a767d4cf1a51969673
jiayupeng <jyp901@hotmail.com>
2022-09-05 13:49:07 +0800

    474. 一和零

---
+++ algorithm/src/main/java/com/jypure/leetcode/Code88.java
@@ -0,0 +1,38 @@
+package com.jypure.leetcode;
+
+/**
+ * 474. 一和零
+ * https://leetcode.cn/problems/ones-and-zeroes/
+ * @author : jiayupeng
+ * @date : 2022/9/5/09:55
+ */
+public class Code88 {
+
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        //dp[i][j]表示i个0和j个1时的最大子集
+        int[][] dp = new int[m + 1][n + 1];
+        int oneNum, zeroNum;
+        for (String str : strs) {
+            oneNum = 0;
+            zeroNum = 0;
+            for (char ch : str.toCharArray()) {
+                if (ch == '0') {
+                    zeroNum++;
+                } else {
+                    oneNum++;
+                }
+            }
+
+            //倒序遍历
+            for (int i = m; i >= zeroNum; i--) {
+                for (int j = n; j >= oneNum; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+}
